36d1d209ac2fea023315a6605c5e2e96dc103fb9
Yuri Maxiutenko <wolfcub@apriorit.com>
2019-04-24 17:15:48 +0300

    remove static library

--- Console/main.cpp
+++ Console/main.cpp
@@ -1,8 +1,12 @@
 ﻿#include <iostream>
+#include "../LibArithmeticStatic/LibArithmeticStatic.Shared/LibArithmeticStatic.h"
 int main()
 {
 	setlocale(LC_ALL, "russian");
-	std::cout << "test" << std::endl;
+	std::cout << "====================DYNAMIC LIBRARY====================" << std::endl;
+    std::cout << "====================DYNAMIC LIBRARY====================" << std::endl;
+    std::cout << "====================STATIC LIBRARY====================" << std::endl;
+    std::cout << "====================STATIC LIBRARY====================" << std::endl;
 	system("pause");
 	return 0;
 }
