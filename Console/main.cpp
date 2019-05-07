7a030049c4940a4d74a6e8f65faaa92c9a43cfb4
Yuri Maxiutenko <wolfcub@apriorit.com>
2019-05-07 10:44:22 +0300

    add all functions, refactor output

--- Console/main.cpp
+++ Console/main.cpp
@@ -1,14 +1,30 @@
 ﻿#include <iostream>
 #include <Windows.h>
-#include "../LibArithmetic/LibArithmetic.h"
+// #include "../LibArithmetic/LibArithmetic.h"
 int main()
 {
+    int a = 12;
+    int b = 3;
     HMODULE hLib = LoadLibrary("LibArithmetic.dll");
-    int(*pFunction)(int, int) = nullptr; // указатель на функцию
-    (FARPROC&)pFunction = GetProcAddress(hLib, "?sum@@YAHHH@Z"); // присвоение адреса функции из dll в дескриптор
-    std:: cout << pFunction(2, 3) << std::endl;
+    int(*pDllSum)(int, int) = nullptr; // указатель на функцию
+    (FARPROC&)pDllSum = GetProcAddress(hLib, "?sum@@YAHHH@Z"); // присвоение адреса функции из dll в
+    
+    int(*pDllSub)(int, int) = nullptr; // указатель на функцию
+    (FARPROC&)pDllSub = GetProcAddress(hLib, "?sub@@YAHHH@Z"); // присвоение адреса функции из dll в
+    
+    int(*pDllMul)(int, int) = nullptr; // указатель на функцию
+    (FARPROC&)pDllMul = GetProcAddress(hLib, "?mul@@YAHHH@Z"); // присвоение адреса функции из dll в
+    
+    int(*pDllDivide)(int, int) = nullptr; // указатель на функцию
+    (FARPROC&)pDllDivide = GetProcAddress(hLib, "?divide@@YAHHH@Z"); // присвоение адреса функции из dll в
+    
     setlocale(LC_ALL, "russian");
+    std::cout << "a = " << a << " b = " << b << std::endl;
     std::cout << "====================DYNAMIC LIBRARY====================" << std::endl;
+    std::cout << "sum: " << pDllSum(12, 3) << std::endl;
+    std::cout << "sub: " << pDllSub(12, 3) << std::endl;
+    std::cout << "mul: " << pDllMul(12, 3) << std::endl;
+    std::cout << "div: " << pDllDivide(12, 3) << std::endl;
     std::cout << "====================DYNAMIC LIBRARY====================" << std::endl;
     std::cout << "====================STATIC LIBRARY====================" << std::endl;
     std::cout << "====================STATIC LIBRARY====================" << std::endl;
