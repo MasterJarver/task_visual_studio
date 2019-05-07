a9c3536b691744a98a5cdc80d08616b6db7a619d
Yuri Maxiutenko <wolfcub@apriorit.com>
2019-05-07 10:28:12 +0300

    get adress and load function

--- Console/main.cpp
+++ Console/main.cpp
@@ -1,10 +1,14 @@
 ﻿#include <iostream>
+#include <Windows.h>
 #include "../LibArithmetic/LibArithmetic.h"
 int main()
 {
-	setlocale(LC_ALL, "russian");
-	std::cout << "====================DYNAMIC LIBRARY====================" << std::endl;
-    std::cout << sum(2, 3) << std::endl;
+    HMODULE hLib = LoadLibrary("LibArithmetic.dll");
+    int(*pFunction)(int, int) = nullptr; // указатель на функцию
+    (FARPROC&)pFunction = GetProcAddress(hLib, "?sum@@YAHHH@Z"); // присвоение адреса функции из dll в дескриптор
+    std:: cout << pFunction(2, 3) << std::endl;
+    setlocale(LC_ALL, "russian");
+    std::cout << "====================DYNAMIC LIBRARY====================" << std::endl;
     std::cout << "====================DYNAMIC LIBRARY====================" << std::endl;
     std::cout << "====================STATIC LIBRARY====================" << std::endl;
     std::cout << "====================STATIC LIBRARY====================" << std::endl;
--- LibArithmetic/LibArithmetic.h
+++ LibArithmetic/LibArithmetic.h
@@ -1,5 +1,5 @@
 #pragma once
-__declspec(dllexport) int sum(int a, int b);
-__declspec(dllexport) int sub(int a, int b);
-__declspec(dllexport) int divide(int a, int b);
-__declspec(dllexport) int mul(int a, int b);
+extern __declspec(dllexport) int sum(int a, int b);
+extern __declspec(dllexport) int sub(int a, int b);
+extern __declspec(dllexport) int divide(int a, int b);
+extern __declspec(dllexport) int mul(int a, int b);
